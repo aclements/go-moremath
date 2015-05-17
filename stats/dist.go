ceb6bc6cc4840c2f6a01f5107ff6f58970563a42
Austin Clements <austin@google.com>
2015-05-17 18:33:58 -0400

    stats: interface for discrete distributions

--- stats/dist.go
+++ stats/dist.go
@@ -34,7 +34,53 @@ type Dist interface {
 	Bounds() (float64, float64)
 }
 
-// TODO: Add a Support method for finite support distributions?
+// A DiscreteDist is a discrete statistical distribution.
+//
+// Most discrete distributions are defined only at integral values of
+// the random variable. However, some are defined at other intervals,
+// so this interface takes a float64 value for the random variable.
+// The probability mass function rounds down to the nearest defined
+// point. Note that float64 values can exactly represent integer
+// values between ±2**53, so this generally shouldn't be an issue for
+// integer-valued distributions (likewise, for half-integer-valued
+// distributions, float64 can exactly represent all values between
+// ±2**52).
+type DiscreteDist interface {
+	// PMF returns the value of the probability mass function
+	// Pr[X = x'], where x' is x rounded down to the nearest
+	// defined point on the distribution.
+	//
+	// Note for implementers: for integer-valued distributions,
+	// round x using int(math.Floor(x)). Do not use int(x), since
+	// that truncates toward zero (unless all x <= 0 are handled
+	// the same).
+	PMF(x float64) float64
+
+	// CDF returns the cumulative probability Pr[X <= x].
+	//
+	// Note that while continuous and discrete probability
+	// distributions differ in how they represent the probability
+	// function, both have continuous cumulative distribution
+	// functions. However, discrete distributions generally have
+	// discontinuous CDFs.
+	CDF(x float64) float64
+
+	// Step returns s, where the distribution is defined for sℕ.
+	Step() float64
+
+	// Bounds returns reasonable bounds for this distribution's
+	// PMF and CDF. Both bounds must be integer multiples of
+	// Step().
+	//
+	// If this distribution has finite support, this must return
+	// exact bounds l, h such that PMF(l')=0 for all l' < l and
+	// PDF(h')=0 for all h' >= h+Step().
+	Bounds() (float64, float64)
+}
+
+// TODO: Add a Support method for finite support distributions? Or
+// maybe just another return value from Bounds indicating that the
+// bounds are exact?
 
 // TODO: Plot method to return a pre-configured Plot object with
 // reasonable bounds and an integral function? Have to distinguish
