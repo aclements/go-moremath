7081bffe2b2f73ca5d8e918ba7c4f3a54b3c8802
Austin Clements <austin@google.com>
2016-03-22 12:25:39 -0400

    fit: unit test for LOESS

--- internal/mathtest/mathtest.go
+++ internal/mathtest/mathtest.go
@@ -12,13 +12,30 @@ import (
 	"testing"
 )
 
-// Aeq returns true if expect and got are equal to 8 significant
-// figures (1 part in 100 million).
+var (
+	aeqDigits int
+	aeqFactor float64
+)
+
+func SetAeqDigits(digits int) int {
+	old := aeqDigits
+	aeqDigits = digits
+	aeqFactor = 1 - math.Pow(10, float64(-digits+1))
+	return old
+}
+
+func init() {
+	SetAeqDigits(8)
+}
+
+// Aeq returns true if expect and got are equal up to the current
+// number of aeq digits set by SetAeqDigits. By default, this is 8
+// significant figures (1 part in 100 million).
 func Aeq(expect, got float64) bool {
 	if expect < 0 && got < 0 {
 		expect, got = -expect, -got
 	}
-	return expect*0.99999999 <= got && got*0.99999999 <= expect
+	return expect*aeqFactor <= got && got*aeqFactor <= expect
 }
 
 func WantFunc(t *testing.T, name string, f func(float64) float64, vals map[float64]float64) {
