f59a5491087f89f265d6d99cdecd1f9260754ac4
Austin Clements <austin@google.com>
2019-05-03 11:41:13 -0400

    graph/*: package documentation

---
+++ graph/graphout/package.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package graphout implements functions to write graphs to common
+// graph formats.
+package graphout
