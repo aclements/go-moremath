891297d4a9b89e2afe69f2c7bebd5098de281347
Austin Clements <austin@google.com>
2019-08-27 09:57:13 -0400

    graph/graphout: support custom dot attributes

--- graph/graphout/dot.go
+++ graph/graphout/dot.go
@@ -23,7 +23,29 @@ type Dot struct {
 	// Label returns the string to use as a label for the given
 	// node. If nil, nodes are labeled with their node numbers.
 	Label func(node int) string
+
+	// NodeAttrs, if non-nil, returns a set of attributes for a
+	// node. If this includes a "label" attribute, it overrides
+	// the label returned by Label.
+	NodeAttrs func(node int) []DotAttr
+
+	// EdgeAttrs, if non-nil, returns a set of attributes for an
+	// edge.
+	EdgeAttrs func(node, edge int) []DotAttr
 }
+
+// DotAttr is an attribute for a Dot node or edge.
+type DotAttr struct {
+	Name string
+	// Val is the value of this attribute. It may be a string
+	// (which will be escaped), bool, int, uint, float64 or
+	// DotLiteral.
+	Val interface{}
+}
+
+// DotLiteral is a string literal that should be passed to dot
+// unescaped.
+type DotLiteral string
 
 func defaultLabel(node int) string {
 	return fmt.Sprintf("%d", node)
@@ -55,14 +77,33 @@ func (d Dot) Fprint(g graph.Graph, w io.Writer) error {
 
 	for i := 0; i < g.NumNodes(); i++ {
 		// Define node.
-		_, err = fmt.Fprintf(w, "n%d [label=%s];\n", i, DotString(label(i)))
+		var attrList []DotAttr
+		var haveLabel bool
+		if d.NodeAttrs != nil {
+			attrList = d.NodeAttrs(i)
+			for _, attr := range attrList {
+				if attr.Name == "label" {
+					haveLabel = true
+					break
+				}
+			}
+		}
+		if !haveLabel {
+			attrList = attrList[:len(attrList):len(attrList)]
+			attrList = append(attrList, DotAttr{"label", label(i)})
+		}
+		_, err = fmt.Fprintf(w, "n%d%s;\n", i, formatAttrs(attrList))
 		if err != nil {
 			return err
 		}
 
 		// Connect node.
-		for _, out := range g.Out(i) {
-			_, err = fmt.Fprintf(w, "n%d -> n%d;\n", i, out)
+		for j, out := range g.Out(i) {
+			var attrs string
+			if d.EdgeAttrs != nil {
+				attrs = formatAttrs(d.EdgeAttrs(i, j))
+			}
+			_, err = fmt.Fprintf(w, "n%d -> n%d%s;\n", i, out, attrs)
 			if err != nil {
 				return err
 			}
@@ -96,4 +137,34 @@ func DotString(s string) string {
 	}
 	buf = append(buf, '"')
 	return string(buf)
+}
+
+// formatAttrs formats attrs as a dot attribute set, including the
+// surrounding brackets. If attrs is empty, it returns an empty
+// string.
+func formatAttrs(attrs []DotAttr) string {
+	if len(attrs) == 0 {
+		return ""
+	}
+	var buf strings.Builder
+	buf.WriteString(" [")
+	for i, attr := range attrs {
+		if i > 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteString(attr.Name)
+		buf.WriteString("=")
+		switch val := attr.Val.(type) {
+		case string:
+			buf.WriteString(DotString(val))
+		case int, uint, float64:
+			fmt.Fprintf(&buf, "%v", val)
+		case DotLiteral:
+			buf.WriteString(string(val))
+		default:
+			panic(fmt.Sprintf("dot attribute %s had unknown type %T", attr.Name, attr.Val))
+		}
+	}
+	buf.WriteString("]")
+	return buf.String()
 }
